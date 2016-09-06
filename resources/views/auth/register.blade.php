2da56d0ff700f46261d55b81097ecffa8f6e8437
Florient Ratel <f.ratel@webetsolutions.com>
2016-09-06 15:48:25 +0000

    Change blank layout for Gentelella support

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,87 +15,34 @@
     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="{{ asset("css/font-awesome.min.css") }}" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{ asset("css/nprogress.css") }}" rel="stylesheet">
     <!-- Custom Theme Style -->
     <link href="{{ asset("css/gentelella.min.css") }}" rel="stylesheet">
+    
+    @stack('stylesheets')
 
 </head>
 
-<body class="login">
-<div class="login_wrapper">
-    <div class="animate form login_form">
-        <section class="login_content">
-            <form method="post" action="{{ url('/register') }}">
-                {!! csrf_field() !!}
-                
-                <h1>Create Account</h1>
-                
-                <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Full Name">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                  <strong>{{ $errors->first('name') }}</strong>
-                </span>
-                    @endif
-                </div>
-                
-                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                  <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                    @endif
-                </div>
-                
-                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" class="form-control" name="password" placeholder="Password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                  <strong>{{ $errors->first('password') }}</strong>
-                </span>
-                    @endif
-                </div>
-                
-                <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                    <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    
-                    @if ($errors->has('password_confirmation'))
-                        <span class="help-block">
-                  <strong>{{ $errors->first('password_confirmation') }}</strong>
-                </span>
-                    @endif
-                </div>
-                <div>
-                    <button class="btn btn-default submit" >Register</button>
-                </div>
-                
-                <div class="clearfix"></div>
-                
-                <div class="separator">
-                    <p class="change_link">Already a member ?
-                        <a href="{{ url('/login') }}" class="to_register"> Log in </a>
-                    </p>
-                    
-                    <div class="clearfix"></div>
-                    <br />
-                    
-                    <div>
-                        <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                        <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
-                    </div>
-                </div>
-            </form>
-        </section>
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        
+        @include('includes/sidebar')
+        
+        @include('includes/topbar')
+        
+        @yield('main_container')
+    
     </div>
 </div>
+
+<!-- jQuery -->
+<script src="{{ asset("js/jquery.min.js") }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset("js/bootstrap.min.js") }}"></script>
+<!-- Custom Theme Scripts -->
+<script src="{{ asset("js/gentelella.min.js") }}"></script>
+
+@stack('scripts')
+
 </body>
 </html>
